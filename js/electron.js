ef0ce4dbd6264a578afe8921d48548bc0d12d9b7
Nate Boldt <nhb15@case.edu>
2022-07-12 19:27:21 -0400

    removing hardware acceleration to see if fixes bug

--- js/electron.js
+++ js/electron.js
@@ -18,6 +18,11 @@ app.commandLine.appendArgument("disable-gpu");
 
 app.commandLine.appendSwitch("headless");
 app.commandLine.appendArgument("headless");
+
+app.commandLine.appendSwitch("disable-software-rasterizer");
+app.commandLine.appendArgument("disable-software-rasterizer");
+
+app.commandLine.appendSwitch("force_high_performance_gpu");
 // Module to create native browser window.
 const BrowserWindow = electron.BrowserWindow;
 
